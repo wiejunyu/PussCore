f76e7a022f9c7d4c183f94a589c7ee6298dcf9f5
puss <673256903@qq.com>
2020-04-17 11:40:57 +0800

    日志模型

---
+++ Puss.Data/Models/Log/TimeLog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puss.Data.Models
+{
+    /// <summary>
+    /// 时间日志
+    /// </summary>
+    public class TimeLog
+    {
+        /// <summary>
+        /// Api路径
+        /// </summary>
+        public string ApiPath { get; set; }
+        /// <summary>
+        /// 时间
+        /// </summary>
+        public long Time { get; set; }
+    }
+}
