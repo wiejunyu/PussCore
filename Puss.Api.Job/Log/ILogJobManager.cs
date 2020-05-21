2262c5f24a643c23c98af479da0e845d478fa37c
puss <673256903@qq.com>
2020-05-21 14:51:24 +0800

    log定时计划

---
+++ Puss.Api.Job/Log/ILogJobManager.cs
@@ -0,0 +1,20 @@
+﻿using Puss.BusinessCore;
+using Puss.Enties;
+using Puss.RabbitMQ;
+using Puss.Data.Models;
+using Newtonsoft.Json;
+using Hangfire;
+
+namespace Puss.Api.Job
+{
+    /// <summary>
+    /// 定时计划日志业务
+    /// </summary>
+    public interface ILogJobManager
+    {
+        /// <summary>
+        /// 定时计划日志业务
+        /// </summary>
+        void Log();
+    }
+}
---
+++ Puss.Api.Job/Log/LogJobManager.cs
@@ -0,0 +1,95 @@
+﻿using Puss.BusinessCore;
+using Puss.Enties;
+using Puss.RabbitMQ;
+using Puss.Data.Models;
+using Newtonsoft.Json;
+using Hangfire;
+
+namespace Puss.Api.Job
+{
+    /// <summary>
+    /// 定时计划日志业务
+    /// </summary>
+    public class LogJobManager : ILogJobManager
+    {
+        /// <summary>
+        /// 定时计划日志业务
+        /// </summary>
+        public void Log()
+        {
+            BackgroundJob.Enqueue(() => ConsumptionLogError());
+            BackgroundJob.Enqueue(() => ConsumptionLogTime());
+            BackgroundJob.Enqueue(() => ConsumptionLogJob());
+        }
+
+        /// <summary>
+        /// 消费控制器错误日志
+        /// </summary>
+        public static void ConsumptionLogError()
+        {
+            RabbitMQPushService RabbitMQPushService = new RabbitMQPushService();
+            while (true)
+            {
+                try
+                {
+                    RabbitMQPushService.PullMessage(QueueKey.LogError, (Message) =>
+                    {
+                        LogErrorDetailsManager LogErrorDetailsManager = new LogErrorDetailsManager();
+                        LogErrorDetailsManager.Insert(JsonConvert.DeserializeObject<LogErrorDetails>(Message));
+                        return true;
+                    });
+                }
+                catch 
+                {
+                    RabbitMQPushService = new RabbitMQPushService();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 消费时间日志
+        /// </summary>
+        public static void ConsumptionLogTime()
+        {
+            RabbitMQPushService RabbitMQPushService = new RabbitMQPushService();
+            while (true)
+            {
+                try
+                {
+                    RabbitMQPushService.PullMessage(QueueKey.LogTime, (Message) =>
+                    {
+                        return true;
+                    });
+                }
+                catch
+                {
+                    RabbitMQPushService = new RabbitMQPushService();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 消费定时计划错误日志
+        /// </summary>
+        public static void ConsumptionLogJob()
+        {
+            RabbitMQPushService RabbitMQPushService = new RabbitMQPushService();
+            while (true)
+            {
+                try
+                {
+                    RabbitMQPushService.PullMessage(QueueKey.LogJob, (Message) =>
+                    {
+                        LogJobDetailsManager LogJobDetailsManager = new LogJobDetailsManager();
+                        LogJobDetailsManager.Insert(JsonConvert.DeserializeObject<LogJobDetails>(Message));
+                        return true;
+                    });
+                }
+                catch
+                {
+                    RabbitMQPushService = new RabbitMQPushService();
+                }
+            }
+        }
+    }
+}
