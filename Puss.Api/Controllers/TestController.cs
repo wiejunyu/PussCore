f6651a6130157e9c2e9bc7c0deb56f88be1cfcbc
puss <673256903@qq.com>
2020-06-03 16:08:18 +0800

    日志优化

--- Puss.Api/Controllers/TestController.cs
+++ Puss.Api/Controllers/TestController.cs
@@ -10,6 +10,9 @@ using Puss.Enties;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Puss.Api.Job;
+using System;
+using System.IO;
+using Puss.Data.Config;
 
 namespace Puss.Api.Controllers
 {
@@ -116,6 +119,28 @@ namespace Puss.Api.Controllers
             {
                 LogJobManager.Log();
                 return new ReturnResult(ReturnResultStatus.Succeed);
+            });
+        }
+
+        /// <summary>
+        /// 获取日志文件
+        /// </summary>
+        /// <param name="type">日志类型 QueueKey</param>
+        /// <param name="date">日志日期 格式：yyyyMMdd，如20200603</param>
+        /// <returns></returns>
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<FileResult> GetLogTxt(string type, string date = null)
+        {
+            return await Task.Run(() =>
+            {
+                if (string.IsNullOrWhiteSpace(date))
+                {
+                    date = DateTime.Now.ToString("yyyyMMdd");
+                }
+                var stream = System.IO.File.OpenRead(Path.Combine(GlobalsConfig.ContentRootPath, $"log/{type}/{date}.TXT"));
+                FileStreamResult result = File(stream, "application/x-apple-aspen-config", $"{date}.TXT");
+                return result;
             });
         }
     }
