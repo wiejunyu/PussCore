c9fbd0fa1656280073b26a231a09f7611f4596c8
韦俊宇 <673256903@qq.com>
2020-09-25 22:12:34 +0800

    更新统计模型、枚举

---
+++ Puss.Api.Manager/Movie/Dto/Enum/UpdateCountStatus.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puss.Data.Enum
+{
+    /// <summary>
+    /// 记录信息状态枚举
+    /// </summary>
+    public enum UpdateCountStatus
+    {
+        /// <summary>
+        /// 开始计算
+        /// </summary>
+        [Description("开始计算")]
+        Start,
+        /// <summary>
+        ///提交数据
+        /// </summary>
+        [Description("提交数据")]
+        Save,
+        /// <summary>
+        ///全部完成
+        /// </summary>
+        [Description("全部完成")]
+        Success,
+        /// <summary>
+        ///报错
+        /// </summary>
+        [Description("报错")]
+        Error,
+    }
+}
---
+++ Puss.Api.Manager/Movie/Dto/Update/UpdateCount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Puss.Api.Manager.MovieManager
+{
+    /// <summary>
+    /// 更新统计
+    /// </summary>
+    public class UpdateCount
+    {
+        /// <summary>
+        /// 总数
+        /// </summary>
+        public int count { get; set; }
+        /// <summary>
+        /// 当前个数
+        /// </summary>
+        public int current { get; set; }
+        /// <summary>
+        /// 当前名称
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 当前状态
+        /// </summary>
+        public int status { get; set; }
+    }
+}
